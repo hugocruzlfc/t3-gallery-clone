95d45e00845478d21b4ce3ff46eec5799ca9cd68
hugocruzlfc <hugocruzlfc@gmail.com>
2024-05-04 12:48:40 +0200

    feat: adding database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import { mockImagesURL } from "../lib/mock-images";
+import { db } from "@/server/db";
 
 const mockImages = mockImagesURL.map((url, id) => ({
   id,
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.name}</h2>
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id}>
             <Image
